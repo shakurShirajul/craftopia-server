be06407df6758208be5844122d7b8115c1f303ab
shakurShirajul <siirajulisllam@gmail.com>
2024-04-27 09:21:28 +0600

    env file added

---
+++ models/items.js
@@ -0,0 +1,38 @@
+import mongoose, { mongo } from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+    image:{
+        type: String,
+        required: true,
+    },
+    item_name:{
+        type: String,
+        required: true,
+    },
+    subcategory_name:{
+        type: String,
+        required: true,
+    },
+    short_descriptiondragon:{
+        type: String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    rating:{
+        type: Number,
+        required: true,
+    },
+    customization:{
+        type: String,
+        required: true,
+    },
+    processing_time:{
+        type: String,
+        required: true,
+    }
+})
+
+export const Items = mongoose.model
