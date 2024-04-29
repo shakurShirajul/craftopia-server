b40d48733431f394f27248cfd64104bfcbc58d34
shakurShirajul <siirajulisllam@gmail.com>
2024-04-29 21:37:29 +0600

    Subcategory Schema Added

---
+++ models/subcategory.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const subcategorySchema = new mongoose.Schema({
+    image: {
+        type: String,
+        required: true,
+    },
+    item_name: {
+        type: String,
+        required: true,
+    },
+    subcategory_name: {
+        type: String,
+        required: true,
+    },
+    short_description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: String,
+        required: true,
+    },
+    customization: {
+        type: String,
+        required: true,
+    },
+    processing_time: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+    },
+    userName: {
+        type: String,
+    }
+})
+
+export const Items = mongoose.model("subcategory", subcategorySchema);
